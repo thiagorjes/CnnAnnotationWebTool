9c658a1252156aafb59e7bf7f63f3d410e239868
thiagorjes <thiagorjes@outlook.com>
2020-01-22 20:54:42 -0300

    coisas novas cityscapes

--- lista.php
+++ lista.php
@@ -2,14 +2,14 @@
 
 function Imagens($id)
 {
-  $file = new SplFileObject("imagens.txt");
+  $file = new SplFileObject("img_cityscapes.txt");
   $file->seek($id);
   return trim(preg_replace('/\s\s+/', ' ', $file->current()));
 }
 
 function Labels($id)
 {
-  $file = new SplFileObject("labels.txt");
+  $file = new SplFileObject("lbl_cityscapes.txt");
   $file->seek($id);
   return trim(preg_replace('/\s\s+/', ' ', $file->current()));
 }
