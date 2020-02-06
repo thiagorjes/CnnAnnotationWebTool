f683ca232f2b0a86fa72a199935ec0c4c606a82e
thiagorjes <thiagorjes@outlook.com>
2020-02-06 05:48:19 -0300

    kitti

--- lista.php
+++ lista.php
@@ -2,14 +2,14 @@
 
 function Imagens($id)
 {
-  $file = new SplFileObject("img_cityscapes.txt");
+  $file = new SplFileObject("img_kitti.txt");
   $file->seek($id);
   return trim(preg_replace('/\s\s+/', ' ', $file->current()));
 }
 
 function Labels($id)
 {
-  $file = new SplFileObject("lbl_cityscapes.txt");
+  $file = new SplFileObject("lbl_kitti.txt");
   $file->seek($id);
   return trim(preg_replace('/\s\s+/', ' ', $file->current()));
 }
